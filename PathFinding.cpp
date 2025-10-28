6cc080d60af127f9c172b3ee1ac745b1523b5069
SergioFrontTest <sergio.dilonardo@gmail.com>
2025-10-28 05:38:58 +0100

    Add files via upload

---
+++ PathFinding.cpp
@@ -0,0 +1,34 @@
+﻿// PathFinding.cpp : Defines the entry point for the application.
+//
+
+#include "PathFinding.h"
+#include "UI/UI.h"
+
+using namespace std;
+
+CPathFindingApp theApp;
+
+BOOL CPathFindingApp::InitInstance()
+{
+	m_pUI = new UI();
+	if (m_pUI)
+	{
+		if (!m_pUI->createWindow(this))
+			return FALSE;
+	}
+
+	return TRUE;
+}
+
+int CPathFindingApp::ExitInstance()
+{
+	int returnValue = CWinApp::ExitInstance();
+
+	if (m_pUI)
+	{
+		delete m_pUI;
+		m_pUI = nullptr;
+	}
+
+	return returnValue;
+}
---
+++ PathFinding.h
@@ -0,0 +1,17 @@
+﻿// Main application process based on CWinApp
+#pragma once
+
+#include <iostream>
+#define _WIN32_WINNT 0x0A00  // Windows 10
+#include <afxwin.h>
+
+class UI;
+
+class CPathFindingApp : public CWinApp
+{
+public:
+	BOOL InitInstance() override;
+	virtual int ExitInstance() override;
+
+	UI* m_pUI = nullptr;
+};
